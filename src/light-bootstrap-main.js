905dd8f1b2b2eede39142b559ad69aeb0f8f4315
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-01-18 01:44:57 +0200

    minor coding style fix

--- src/light-bootstrap-main.js
+++ src/light-bootstrap-main.js
@@ -1,6 +1,11 @@
 import VTooltip from 'v-tooltip';
 // Notifications plugin
 import Notifications from 'vue-notifyjs';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'vue-notifyjs/themes/default.scss';
+import 'es6-promise/auto';
+
 // A plugin file where you could register global components used across the app
 import GlobalComponents from './globalComponents';
 // A plugin file where you could register global directives
@@ -9,17 +14,11 @@ import GlobalDirectives from './globalDirectives';
 import SideBar from './components/UIComponents/SidebarPlugin';
 
 // asset imports
-import 'bootstrap/dist/css/bootstrap.css';
-import 'vue-notifyjs/themes/default.scss';
 import './assets/sass/light-bootstrap-dashboard.scss';
 import './assets/css/demo.css';
 
 // library auto imports
-import 'es6-promise/auto';
 
-/**
- * This is the main Light Bootstrap Dashboard Vue plugin where dashboard related plugins are registerd.
- */
 export default {
   install(Vue) {
     Vue.use(GlobalComponents);
