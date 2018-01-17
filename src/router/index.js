09ae365a7c8b8a4d43e6e23defbefc0c37bedd48
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-01-17 22:05:57 +0200

    adds routes and compoenets from theme

--- src/router/index.js
+++ src/router/index.js
@@ -6,12 +6,25 @@ import Groups from '@/components/Groups';
 import OUnits from '@/components/OrganizationalUnits';
 import Security from '@/components/Security';
 
+import DashboardLayout from '@/components/Dashboard/Layout/DashboardLayout';
+// GeneralViews
+import NotFound from '@/components/GeneralViews/NotFoundPage';
+
+// Admin pages
+import Overview from '@/components/Dashboard/Views/Overview';
+import UserProfile from '@/components/Dashboard/Views/UserProfile';
+import TableList from '@/components/Dashboard/Views/TableList';
+import Typography from '@/components/Dashboard/Views/Typography';
+import Icons from '@/components/Dashboard/Views/Icons';
+import Maps from '@/components/Dashboard/Views/Maps';
+import Notifications from '@/components/Dashboard/Views/Notifications';
+
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/main',
       name: 'main',
       component: Main,
     },
@@ -35,5 +48,53 @@ export default new Router({
       name: 'security',
       component: Security,
     },
+    {
+      path: '/',
+      component: DashboardLayout,
+      redirect: '/admin/overview',
+    },
+    {
+      path: '/admin',
+      component: DashboardLayout,
+      redirect: '/admin/overview',
+      children: [
+        {
+          path: 'overview',
+          name: 'Overview',
+          component: Overview,
+        },
+        {
+          path: 'user',
+          name: 'User',
+          component: UserProfile,
+        },
+        {
+          path: 'table-list',
+          name: 'Table List',
+          component: TableList,
+        },
+        {
+          path: 'typography',
+          name: 'Typography',
+          component: Typography,
+        },
+        {
+          path: 'icons',
+          name: 'Icons',
+          component: Icons,
+        },
+        {
+          path: 'maps',
+          name: 'Maps',
+          component: Maps,
+        },
+        {
+          path: 'notifications',
+          name: 'Notifications',
+          component: Notifications,
+        },
+      ],
+    },
+    { path: '*', component: NotFound },
   ],
 });
